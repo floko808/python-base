d6621e0ba06790e3e96ec690533fea440023fde5
Fabio Barros <ph4byus@gmail.com>
2022-08-14 18:58:18 -0300

    Hello World Multi Linguas 0.0.1

--- hello.py
+++ hello.py
@@ -1,2 +1,33 @@
 #!/usr/bin/env python3
-print("Jujuca Chatolina!".upper())
+"""Hello World Multi Linguas.
+
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada. Exemplo:
+
+    export LANG=pt_BR
+
+Execução:
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Fabio Barros"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+else:
+    msg = "Hello, World!"
+
+print(msg)
